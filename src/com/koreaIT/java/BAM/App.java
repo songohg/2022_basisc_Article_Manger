44cdae9c0c79a09cd60c01398ce71d1b0facf359
songohg <songogh94@gmail.com>
2022-09-20 21:00:03 +0900

    article list에 검색 기능 추가

--- src/com/koreaIT/java/BAM/App.java
+++ src/com/koreaIT/java/BAM/App.java
@@ -50,17 +50,37 @@ public class App {
 
 				System.out.printf("%d번 글이 생성되었습니다\n", id);
 
-			} else if (cmd.equals("article list")) {
+			} else if (cmd.startsWith("article list")) {
 				if (articles.size() == 0) {
 					System.out.println("게시물이 없습니다");
 					continue;
 				}
 
-		
+				List<Article> forPrintArticles = articles;
 				
+				String searchKeyword = cmd.substring("article list".length()).trim();	
+				
+				if(searchKeyword.length() > 0) {
+					
+				System.out.println("검색어 : " + searchKeyword);
+				
+				forPrintArticles = new ArrayList<>();
+				
+				for(Article article : articles) {					
+					if(article.title.contains(searchKeyword)) {
+						forPrintArticles.add(article);
+					}
+				}
+					if(forPrintArticles.size() == 0) {
+						System.out.println("검색결과가 없습니다");
+						continue;
+					}
+				}
+							
 				System.out.println("번호	|	제목	|	날짜			|	조회");
-				for (int i = articles.size() - 1; i >= 0; i--) {
-					Article article = articles.get(i);
+				
+				for (int i = forPrintArticles.size() - 1; i >= 0; i--) {
+					Article article = forPrintArticles.get(i);
 					System.out.printf("%d	|	%s	|	%s	|	%d\n", article.id, article.title, article.regDate,
 							article.viewCnt);
 				}
@@ -135,7 +155,7 @@ public class App {
 	private int getArticleIndexById(int id) {
 		int i = 0;
 		
-		for(Article artcle : articles) {
+		for(Article article : articles) {
 			if (article.id == id) {
 					return i;
 			}
