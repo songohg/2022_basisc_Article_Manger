b2ac63bd65183c04ff5557845274635da59ece21
songohg <songogh94@gmail.com>
2022-09-20 16:41:39 +0900

    게시물 수정 기능 구현

--- src/com/koreaIT/java/BAM/Main.java
+++ src/com/koreaIT/java/BAM/Main.java
@@ -47,8 +47,7 @@ public class Main {
 								
 				System.out.printf("%d번 글이 생성되었습니다\n", id);
 				
-			}else if(cmd.equals("article list")) {
-				
+			}else if(cmd.equals("article list")) {				
 				if(articles.size()==0) {					
 				System.out.println("게시물이 없습니다.");
 				continue;
@@ -86,7 +85,39 @@ public class Main {
 					System.out.printf("제목 : %s\n", foundArticle.title);
 					System.out.printf("내용 : %s\n", foundArticle.body);					
 								
-			  } else if (cmd.startsWith("article delete ")) {
+			  } else if (cmd.startsWith("article modify ")) {
+					
+					String[] cmdBits = cmd.split("");
+					int id = Integer.parseInt(cmdBits[2]);
+							 			
+					Article foundArticle = null;
+									
+					for(int i = 0; i < articles.size(); i++) {
+						Article article = articles.get(i);
+						
+						if(article.id == id) {
+							 
+							foundArticle = article;
+							break;
+						}					
+					}
+					
+					if(foundArticle == null) {
+						System.out.printf("%d번 게시물은 존재하지 않습니다\n", id);
+						continue;
+					}	
+					
+					System.out.printf("수정할 제목 : ");
+					String title = sc.nextLine();
+					System.out.printf("수정할 내용 : ");
+					String body = sc.nextLine();
+					
+					foundArticle.title = title;
+					foundArticle.body = body;
+					
+					System.out.printf("%d번글이 수정되었습니다\n", id);
+					
+				  } else if (cmd.startsWith("article delete ")) {
 
 				String[] cmdBits = cmd.split(" ");
 				int id = Integer.parseInt(cmdBits[2]);
